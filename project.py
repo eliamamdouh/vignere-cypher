c4ee7719660ef48ff428f7d742e2b52518fa45da
eliamamdouh <114479644+eliamamdouh@users.noreply.github.com>
2022-10-28 21:35:02 +0200

    final project

--- project.py
+++ project.py
@@ -37,8 +37,8 @@ def decrypt(c, k):
     return p
 try:
     print("Welcome to vigenere cipher.\n\n"
-          "The text message should contain CHARACTERS AND NUMBERS and the key should be one character word \n"
-          "Press 1 to Enrypt a message \npress 2 to Decrypt a message")
+          "The text message should contain letter and numbers and special characters and the key should be one character word \n"
+          " Press 1 to Enrypt a message \n press 2 to Decrypt a message\n press 3 to encrypt through a file \n press 4 to decrypt through a file")
     choose = input("Choice: ")
     if choose == '1':
        p = input("enter the plain text: ")
@@ -60,8 +60,38 @@ try:
         else:
             p = decrypt (c,k)
             print("the plain text is:",p)
+
+    elif choose == "3":
+       f = input("enter the file name:")# open the file using open() function
+       f = f.replace(" ", "")
+       o = input("enter the plain text: ")
+       o = o.replace(" ", "")
+       k = input("enter the key:")
+       file = open(f,"w")# Overwrite the file
+       file.write(encrypt(o,k))
+       if k.isalpha():
+        c = encrypt(o,k)
+        print("the cipher text is",c)
+       else:
+        print(k)
+        print("enter valid key,key is only one character word")
+    elif choose == "4":
+         f = input("enter the file name:")# open the file using open() function
+         f = f.replace(" ", "")
+         c = input("enter the encrypted text: ")
+         c = c.replace(" ", "")
+         k = input("enter the key:")
+         file = open(f,"w")# Overwrite the file
+         file.write(decrypt(c,k))
+         if not k.isalpha():#isaplha check if the input is only letters
+            print("enter valid key,key is only one word ")
+         else:
+            p = decrypt (c,k)
+            print("the plain text is:",p)
     else:
         print("please enter a valid choice")
 except exception as e:
     print(e)
     exit("enter a valid text please")
+
+    
---
+++ test_code.py
@@ -0,0 +1,5 @@
+# open the file using open() function
+file = open("text file.txt", 'w')
+
+# Overwrite the file
+file.write(" All content has been overwritten !")
